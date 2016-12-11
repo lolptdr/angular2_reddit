5f81ec3bebc88a00897cd39101051ee2bbbc916e
Joseph Tingsanchali <jtingsan@gmail.com>
2016-12-10 22:53:53 -0600

    Add article component to allow voteUp and voteDown (stop click event propagation

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
